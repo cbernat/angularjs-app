f957b73c68ec4f4a4899d7cde88d35d68a25e7ff
cbernat <cbernat@makingsense.com>
2014-12-18 17:17:15 -0300

    new changes

--- app/controllers/productListControllers.js
+++ app/controllers/productListControllers.js
@@ -1,11 +1,25 @@
 angular.module("sportsStore")
-.controller("productListCtrl", function ($scope, $filter) {
+.constant("productListActiveClass", "btn-primary")
+.constant("productListPageCount", 3)
+.controller("productListCtrl", function ($scope, $filter, productListActiveClass,productListPageCount) {
+	$scope.selectedPage = 1;
+	$scope.pageSize = productListPageCount;
 var selectedCategory = null;
-$scope.selectCategory = function (newCategory) {
-selectedCategory = newCategory;
-}
-$scope.categoryFilterFn = function (product) {
-return selectedCategory == null ||
-product.category == selectedCategory;
-}
+	$scope.selectCategory = function (newCategory) {
+		selectedCategory = newCategory;
+		$scope.selectedPage = 1;
+	}
+	$scope.selectPage = function (newPage) {
+		$scope.selectedPage = newPage;
+	}
+	$scope.categoryFilterFn = function (product) {
+		return selectedCategory == null ||
+		product.category == selectedCategory;
+	}
+	$scope.getCategoryClass = function (category) {
+		return selectedCategory == category ? productListActiveClass : "";
+	}
+	$scope.getPageClass = function (page) {
+	return $scope.selectedPage == page ? productListActiveClass : "";
+	}
 });
--- app/controllers/sportsStore.js
+++ app/controllers/sportsStore.js
@@ -1,28 +1,17 @@
 angular.module("sportsStore")
-.controller("sportsStoreCtrl", function ($scope) {
-	$scope.data = {
-	products: [
-		{ name: "Product #1", description: "A product",
-		category: "Category #1", price: 100 },
-		{ name: "Product #2", description: "A product",
-		category: "Category #1", price: 110 },
-		{ name: "Product #3", description: "A product",
-		category: "Category #2", price: 210 },
-		{ name: "Product #4", description: "A product",
-		category: "Category #3", price: 202 }]
-	};
-})
-.constant("productListActiveClass", "btn-primary")
-.controller("productListCtrl", function ($scope, $filter, productListActiveClass) {
-var selectedCategory = null;
-$scope.selectCategory = function (newCategory) {
-selectedCategory = newCategory;
-}
-$scope.categoryFilterFn = function (product) {
-return selectedCategory == null ||
-product.category == selectedCategory;
-}
-$scope.getCategoryClass = function (category) {
-return selectedCategory == category ? productListActiveClass : "";
-}
+.constant("dataUrl", "http://localhost:2403/products?callback=jsonp_callback")
+.controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+	
+$scope.data = {};
+$scope.data.products = {};
+$scope.data.products = [{"Name":"Kayak","description":"A boat for one person","category":"Watersports","price":275,"id":"91300bc73f45c82b"},{"Name":"Lifejacket","description":"Protective and fashionable","category":"Watersports","price":48.95,"id":"7f6e52d104de78b3"},{"Name":"Soccer Ball","description":"FIFA-approved size and weight","category":"Soccer","price":19.5,"id":"d73e7efbf518b8a2"},{"Name":"Corner Flags","description":"Give your playing field a professional touch","category":"Soccer","price":34.95,"id":"a8ede2ba8fcedb65"},{"Name":"Stadium","description":"Flat-packed 35,000-seat stadium","category":"Soccer","price":79500,"id":"39c8eeabf901a893"},{"Name":"Thinking Cap ","description":"Improve your brain efficiency by 75%","category":"Chess","price":16,"id":"78ad967eab3ff8ff"},{"Name":"Unsteady Chair","description":"Secretly give your opponent a disadvantage","category":"Chess","price":29.95,"id":"311bd783f36d38d1"},{"Name":"Human Chess Board","description":"A fun game for the family","category":"Chess","price":75,"id":"fd17324b62d6c820"},{"Name":"Bling-Bling King","description":"Gold-plated, diamond-studded King","category":"Chess","price":1200,"id":"582163c10a0cf8c2"}];
+	$http.jsonp(dataUrl). //function gave error
+  success(function(data) {
+    $scope.data.products2 = data;
+    
+  })
+  .error(function (error, status, headers, config) {
+    $scope.data.error = error;
+  });
+
 });
